70b3be21ebaaab836bac7b080e19342a5ddc3659
ali bagherian <ali_bghrn2006@yahoo.com>
2025-01-17 18:33:21 +0000

    add

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 function MovieDetail({ movie }) {
   const [details, setDetails] = useState(null);
-
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`).then((response) => {
       setDetails(response.data);
